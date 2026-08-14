7beac6b74088eb227b95c9e801b2ea3156015643
Wil Nguyen <wiln4789@gmail.com>
2026-08-14 19:41:06 -0400

    initial commit

---
+++ morse_code_led.ino
@@ -0,0 +1,15 @@
+// A time unit is the smallest unit of time used in Morse Code.
+// The time unit is measured in milliseconds.
+// The duration of every sound or break is a multiple of the time unit.
+// Dots are 1 time unit.
+// Dashes are 3 time units.
+// Breaks between dots and dashes within a letter are 1 time unit.
+// Breaks between letters are 3 time units.
+// Breaks between words are 7 time units. 
+const int time_unit = 400;
+
+void setup() {
+}
+
+void loop() {
+}
